ff8d2f340c8b45edeae1fae0ea5b7c957e9840e5
jigneshz <jignesh.zinzuwadia@gmail.com>
2019-06-26 16:47:24 +1000

    unwanted cleanup and add pythonlearning

---
+++ Pythonlearning/funct1.py
@@ -0,0 +1,9 @@
+def facts(y):
+    result=1
+    while y>=1:
+        result=result*y
+        y=y-1
+    return result
+
+for i in range(1,6)
+    print("The result of {} is: {}".format(i,facts(i)))
